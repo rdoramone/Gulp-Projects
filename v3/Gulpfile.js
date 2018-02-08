cb00ac9a35e6d11e9ee2165a9dcfa8c391ce823a
Ricardo Jesus <rdo_ramone@hotmail.com>
2018-02-08 19:36:18 -0200

    automatização das tasks, varredura das tasks de prod, dev e watch em cada arquivo e inserção em um array, para passar como parâmetro na task de build

--- v3/Gulpfile.js
+++ v3/Gulpfile.js
@@ -1,17 +1,44 @@
+var fs = require('fs');
 var pathTasks = './config/gulp/tasks/';
-
-/* Chamando apenas as task a serem utilizadas, uma a uma */
-// require(pathTasks + 'app');
-// require(pathTasks + 'build');
-// require(pathTasks + 'fonts');
-// require(pathTasks + 'html');
-// require(pathTasks + 'image');
-// require(pathTasks + 'libs');
-// require(pathTasks + 'sass');
-// require(pathTasks + 'utils');
+var tasksWatch = ['browserSyncDev'];
+var tasksDev = [];
+var tasksProd = [];
+var regExpDev = /(gulp.task\('?"?)([a-zA-Z]+Dev)/;
+var regExpProd = /(gulp.task\('?"?)([a-zA-Z]+Prod)/;
+var regExpWatch = /(gulp.task\('?"?)(watch[a-zA-Z]+)/;
 
 /* Chamando apenas as task a serem utilizadas, verificando os arquivos que estão na pasta task */
-require('fs').readdirSync(pathTasks).forEach((file) => {
-        if (file !== 'compass.js') 
-            return require(pathTasks + file);
+fs.readdirSync(pathTasks).forEach((file) => {
+    if (file !== 'compass.js') {
+        var contentTasks = fs.readFileSync(pathTasks + file,'utf-8');
+        var resultDev = regExpDev.exec(contentTasks);
+        var resultProd = regExpProd.exec(contentTasks);
+        var resultWatch = regExpWatch.exec(contentTasks);
+
+        if (regExpDev.test(contentTasks)) {
+            tasksDev.push(resultDev[2]);
+        }
+
+        if (regExpProd.test(contentTasks)) {
+            tasksProd.push(resultProd[2]);
+        }
+
+        if (regExpWatch.test(contentTasks)) {
+            tasksWatch.push(resultWatch[2]);
+        }
+
+        return require(pathTasks + file);
+    }
 });
+
+module.exports = {
+    tasks: {
+        dev: tasksDev,
+        prod: tasksProd,
+        watch: tasksWatch,
+        browserSync: {
+            dev: 'browserSyncDev',
+            prod: 'browserSyncProd'
+        }
+    }
+}
